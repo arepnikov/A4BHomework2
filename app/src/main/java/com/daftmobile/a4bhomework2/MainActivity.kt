2ed24d72ee015f35327b8213ba875fc9a06673f4
Sir Alexsearch <aleksander.repnikov@gmail.com>
2018-11-18 23:20:21 +0100

    3 buttons with onClickListener for textView

--- app/src/main/java/com/daftmobile/a4bhomework2/MainActivity.kt
+++ app/src/main/java/com/daftmobile/a4bhomework2/MainActivity.kt
@@ -2,11 +2,25 @@ package com.daftmobile.a4bhomework2
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener { buttonOnClickListener(it as Button) }
+        button2.setOnClickListener { buttonOnClickListener(it as Button) }
+        button3.setOnClickListener { buttonOnClickListener(it as Button) }
+    }
+
+    private
+
+    fun buttonOnClickListener( button: Button ) {
+        textView.text = button.text
     }
 }
+
